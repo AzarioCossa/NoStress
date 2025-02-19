50873f48e01b516d569a09b23022f94c188a92f6
AzarioCossa <cossaazario@gmail.com>
2025-02-19 22:47:48 +0100

    implementation on text generator

---
+++ src/models/text.structure.ts
@@ -0,0 +1,8 @@
+export class TextStructure {
+    structure: string = "Capa, Contracapa, Resumo, Sumário, Introdução, Objetivos, Revisão da Literatura, Metodologia, Resultados, Discussão, Conclusão, Bibliografia, Anexos (se necessário)";
+
+    getStructure(): string {
+        return this.structure;
+    }
+}
+
