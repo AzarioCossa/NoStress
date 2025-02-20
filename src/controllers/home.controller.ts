d25de4dcbc970cca3c2689d7e550d273f21e53f4
AzarioCossa <cossaazario@gmail.com>
2025-02-20 01:24:08 +0100

    add mainPage styling

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from 'express';
-import { TextGenerator } from '../models/text.generator';
+//import { TextGenerator } from '../models/text.generator';
 
 export default class HomeController {
     async renderHome(req: Request, res: Response): Promise<void> {
         try {
-            const generator = new TextGenerator('Inteligência Artificial', 5);
-            const content = await generator.generateContent();
-
-            res.render('index', { content });
+            /*const generator = new TextGenerator('Inteligência Artificial', 5);
+            const content = await generator.generateContent();*/
+            res.render('index');
         } catch (error) {
             console.error('Error generating content:', error);
             res.status(500).send('Internal Server Error');
